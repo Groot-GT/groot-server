3c3ed56fa4360f3424536219bb9e5ae7de78b89a
Go-Jaecheol <gojaech@naver.com>
2023-03-26 15:43:23 +0900

    chore(MS-13): 스프링 부트 프로젝트 기초 설정

---
+++ src/main/java/com/groot/mindmap/MindmapApplication.java
@@ -0,0 +1,13 @@
+package com.groot.mindmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmapApplication.class, args);
+	}
+
+}
