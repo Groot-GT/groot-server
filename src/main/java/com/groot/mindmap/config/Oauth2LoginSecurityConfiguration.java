1e94c0fabc1010cf9df64371d04ab83f5cc42e42
ONE <31034469+choco-one@users.noreply.github.com>
2023-05-17 16:49:13 +0900

    feat(MS-38): kakao & naver 로그인 기능 추가 및 소셜 서버로 부터 받은 사용자 정보를 가공하는 로직 추가

--- src/main/java/com/groot/mindmap/config/Oauth2LoginSecurityConfiguration.java
+++ src/main/java/com/groot/mindmap/config/Oauth2LoginSecurityConfiguration.java
@@ -1,19 +1,23 @@
 package com.groot.mindmap.config;
 
+import com.groot.mindmap.user.domain.AuthInformation;
+import com.groot.mindmap.user.domain.AuthInformationFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.util.Assert;
 
 @Slf4j
 @Configuration
@@ -42,20 +46,21 @@ public class Oauth2LoginSecurityConfiguration {
 
     private AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
         return (request, response, authentication) -> {
-            DefaultOAuth2User auth2User = (DefaultOAuth2User) authentication.getPrincipal();
-            log.info("Principal: {}", auth2User);
+            OAuth2User auth2User = (DefaultOAuth2User) authentication.getPrincipal();
 
-            String name = auth2User.getAttribute("name");
-            String email = auth2User.getAttribute("email");
-            String picture = auth2User.getAttribute("picture");
+            // Get the platform name
+            String platform = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+            Assert.notNull(platform, "platform can not be null");
 
-            log.info("name: {}", name);
-            log.info("email: {}", email);
-            log.info("picture: {}", picture);
+            AuthInformation information = AuthInformationFactory.valueOf(platform.toUpperCase()).create(auth2User);
+
+            response.sendRedirect("/login?success=true");
         };
     }
 
     private AuthenticationFailureHandler oAuth2AuthenticationFailureHandler() {
-        return new SimpleUrlAuthenticationFailureHandler("/login?error=true");
+        return (request, response, exception) -> {
+            response.sendRedirect("/login?error=true");
+        };
     }
 }
