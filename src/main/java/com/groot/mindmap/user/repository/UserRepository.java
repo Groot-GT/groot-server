c3809ec0dc48e03616105f2254a5ee7ba3235c01
ONE <31034469+choco-one@users.noreply.github.com>
2023-05-18 16:44:59 +0900

    feat(MS-37): 최초 로그인 시 유저를 생성하는 기능 추가

--- src/main/java/com/groot/mindmap/user/repository/UserRepository.java
+++ src/main/java/com/groot/mindmap/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.groot.mindmap.user.repository;
 import com.groot.mindmap.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByPlatformAndEmail(String platform, String email);
 }
--- src/main/java/com/groot/mindmap/user/service/UserService.java
+++ src/main/java/com/groot/mindmap/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.groot.mindmap.user.service;
 
+import com.groot.mindmap.user.domain.AuthInformation;
 import com.groot.mindmap.user.domain.User;
 import com.groot.mindmap.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,12 +16,18 @@ public class UserService {
     private final UserRepository repository;
 
     @Transactional
-    public Long create(final String name, final String email, final String profileImage) {
+    public Long create(final AuthInformation information) {
         User user = User.builder()
-                .name(name)
-                .email(email)
-                .profileImage(profileImage)
+                .name(information.name())
+                .email(information.email())
+                .profileImage(information.profileImage())
+                .platform(information.platform())
                 .build();
         return repository.save(user).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> findByPlatformAndEmail(final String platform, final String email) {
+        return repository.findByPlatformAndEmail(platform, email);
     }
 }
