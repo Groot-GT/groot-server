3c79aa1eb98a5e27e87610e805dde574249792d2
ONE <31034469+choco-one@users.noreply.github.com>
2023-05-23 12:41:34 +0900

    feat(MS-27): Node List 메세지에서 리스트가 아닌 Key-Value로 반환하도록 변경

--- src/main/java/com/groot/mindmap/message/domain/NodeListMessage.java
+++ src/main/java/com/groot/mindmap/message/domain/NodeListMessage.java
@@ -4,14 +4,17 @@ import com.groot.mindmap.node.domain.Node;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Getter
 public class NodeListMessage extends Message {
 
-    private final List<Node> nodes;
+    private final Map<Long, Node> nodes;
 
     public NodeListMessage(Message message, List<Node> nodes) {
         super(message.getPageId(), message.getType());
-        this.nodes = nodes;
+        this.nodes = nodes.stream().collect(Collectors.toMap(Node::getId, Function.identity()));
     }
 }
