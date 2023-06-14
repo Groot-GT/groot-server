4df0b5d0c327c2000688a506ab6027a4215f0768
ONE <31034469+choco-one@users.noreply.github.com>
2023-06-14 17:13:51 +0900

    bug(develop): 기존의 List를 Map으로 바꾸면서 발생한 Test 오류를 수정

--- src/test/java/com/groot/mindmap/node/controller/NodeControllerTest.java
+++ src/test/java/com/groot/mindmap/node/controller/NodeControllerTest.java
@@ -1,30 +1,9 @@
 package com.groot.mindmap.node.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.groot.mindmap.node.dto.NodeRequest;
 import com.groot.mindmap.node.dto.NodeResponse;
 import com.groot.mindmap.node.service.NodeService;
-import java.util.Collections;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -35,6 +14,23 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
@@ -86,7 +82,7 @@ class NodeControllerTest {
         final NodeResponse nodeResponse1 = new NodeResponse(1L, "노드1", "노드1입니다.", "#FFFFFF", null, List.of(2L));
         final NodeResponse nodeResponse2 = new NodeResponse(2L, "노드2", "노드2입니다.", "#000000", 1L,
                 Collections.emptyList());
-        final List<NodeResponse> response = List.of(nodeResponse1, nodeResponse2);
+        final Map<Long, NodeResponse> response = Map.of(nodeResponse1.id(), nodeResponse1, nodeResponse2.id(), nodeResponse2);
         given(nodeService.list(pageId)).willReturn(response);
 
         // when & then
@@ -100,13 +96,22 @@ class NodeControllerTest {
                                 parameterWithName("pageId").description("Page ID")
                         ),
                         responseFields(
-                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("Node ID"),
-                                fieldWithPath("[].title").type(JsonFieldType.STRING).description("Node 제목"),
-                                fieldWithPath("[].content").type(JsonFieldType.STRING).description("Node 내용"),
-                                fieldWithPath("[].color").type(JsonFieldType.STRING).description("Node 색깔"),
-                                fieldWithPath("[].parentId").type(JsonFieldType.NUMBER).description("부모 Node ID")
+                                subsectionWithPath("1").description("Node ID"),
+                                fieldWithPath("1.id").type(JsonFieldType.NUMBER).description("Node ID"),
+                                fieldWithPath("1.title").type(JsonFieldType.STRING).description("Node 제목"),
+                                fieldWithPath("1.content").type(JsonFieldType.STRING).description("Node 내용"),
+                                fieldWithPath("1.color").type(JsonFieldType.STRING).description("Node 색깔"),
+                                fieldWithPath("1.parentId").type(JsonFieldType.NUMBER).description("부모 Node ID")
                                         .optional(),
-                                fieldWithPath("[].children").type(JsonFieldType.ARRAY).description("자식 Node 목록")
+                                fieldWithPath("1.children").type(JsonFieldType.ARRAY).description("자식 Node 목록"),
+                                subsectionWithPath("2").description("Node ID"),
+                                fieldWithPath("2.id").type(JsonFieldType.NUMBER).description("Node ID"),
+                                fieldWithPath("2.title").type(JsonFieldType.STRING).description("Node 제목"),
+                                fieldWithPath("2.content").type(JsonFieldType.STRING).description("Node 내용"),
+                                fieldWithPath("2.color").type(JsonFieldType.STRING).description("Node 색깔"),
+                                fieldWithPath("2.parentId").type(JsonFieldType.NUMBER).description("부모 Node ID")
+                                        .optional(),
+                                fieldWithPath("2.children").type(JsonFieldType.ARRAY).description("자식 Node 목록")
                         )
                 )).andExpect(status().isOk());
     }
